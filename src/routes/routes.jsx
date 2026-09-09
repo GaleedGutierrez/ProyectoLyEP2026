7ff931483c5ccae7883c3d7fd83129f0b0fe50f4
enzov <enzovilla1810@gmail.com>
2026-09-09 12:18:44 -0300

    refactor: Simplificar rutas eliminando RutaProtegida y reorganizando componentes de layout

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,42 +1,25 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Login from '../pages/Login'
 import Dashboard from '../pages/Dashboard'
 import ListaClientes from '../pages/ListaClientes'
 import DetalleCliente from '../pages/DetalleCliente'
 import ErrorPage from '../pages/ErrorPage'
-import RutaProtegida from '../components/RutaProtegida'
+import PublicLayout from '../layouts/PublicLayout'
+import PrivateLayout from '../layouts/PrivateLayout'
+
 const AppRoutes = () => {
   return (
     <Routes>
-
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <RutaProtegida>
-            <Dashboard />
-          </RutaProtegida>
-        }
-      />
-      <Route
-        path="/clientes"
-        element={
-          <RutaProtegida>
-            <ListaClientes />
-          </RutaProtegida>
-        }
-      />
-      <Route
-        path="/clientes/:id"
-        element={
-         <RutaProtegida>
-          <DetalleCliente />
-         </RutaProtegida>
-      }
-      />
-      <Route path="*" element={<ErrorPage />} />
-
+      <Route element={<PublicLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+      <Route element={<PrivateLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/clientes" element={<ListaClientes />} />
+        <Route path="/clientes/:id" element={<DetalleCliente />} />
+      </Route>
     </Routes>
   )
 }
