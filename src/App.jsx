ce66be1b107bca04989ca8f8d6b518da6d6dfd6a
enzov <enzovilla1810@gmail.com>
2026-09-09 12:19:00 -0300

    refactor: Eliminar componentes de Header, Nav y Footer en la vista de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import '../src/css/app.css'
-import Header from './components/Header'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
 import AppRoutes from './routes/routes'
-import useAutorizaciones from './hooks/useAutorizaciones'
 
 function App() {
-return (
-<>
-    <Header />
-      <Nav />
-        <AppRoutes />
-    <Footer />
-</>
-)
+  return <AppRoutes />
 }
 export default App
