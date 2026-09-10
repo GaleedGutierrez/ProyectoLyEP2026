d0dce10213c9118855f64c5b0314181f3cd400d5
Rodrigo Guanactolay <guanctolayrodrigo6@gmail.com>
2026-09-10 18:04:53 -0300

    fix: ajustar header.jsx para diseño responsivo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,45 @@
 import '../css/header.css'
 import { Navbar, Container, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import useAutorizaciones  from "../hooks/useAutorizaciones";
+import useAutorizaciones from "../hooks/useAutorizaciones";
 
 const Header = () => {
     const { admin, cerrarSesion } = useAutorizaciones();
-    const navigate=useNavigate()
-    const manejarCerrarSesion=()=>{
-        cerrarSesion()
-        navigate ('/login')
-    }
+    const navigate = useNavigate();
+
+    const manejarCerrarSesion = () => {
+        cerrarSesion();
+        navigate('/login');
+    };
+
     return (
-    <Navbar>
-        <Container>
-            <Navbar.Brand>
-                Panel de Control de Clientes
-            </Navbar.Brand>
-            {
-                admin && (
+        <Navbar className="header-principal">
+            <Container className="header-container">
+
+                <Navbar.Brand className="header-titulo">
+                    Panel de Control de Clientes
+                </Navbar.Brand>
+
+                {admin && (
                     <div className="usuario-header">
-                       <p>
-                         {admin.nombre} - {admin.sector}
-                     </p>
-                        <Button className="btn-header"
-                         onClick={manejarCerrarSesion}
-                        > Cerrar Sesion </Button>
+
+                        <p className="usuario-info">
+                            {admin.nombre} - {admin.sector}
+                        </p>
+
+                        <Button
+                            className="btn-header"
+                            onClick={manejarCerrarSesion}
+                        >
+                            Cerrar Sesión
+                        </Button>
+
                     </div>
-                )
-            }
-        </Container>
-    </Navbar>
-               
-    )
-}
+                )}
+
+            </Container>
+        </Navbar>
+    );
+};
+
 export default Header;
