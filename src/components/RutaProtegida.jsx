6870257ae28691a7855d201f50b2132026494699
galeed gutierrez <galeedeveloper@gmail.com>
2026-09-13 16:06:07 -0300

    chore(rutaProtegida): format code and improve readability
    * Standardize import statements
    * Enhance component structure for better clarity

--- src/components/RutaProtegida.jsx
+++ src/components/RutaProtegida.jsx
@@ -1,12 +1,26 @@
-import { Navigate } from 'react-router-dom'
-import useAutorizaciones from '../hooks/useAutorizaciones'
+import { Navigate } from 'react-router-dom';
 
+import useAutorizaciones from '../hooks/useAutorizaciones';
+
+/**
+ * A component that protects a route by checking if the user is authenticated.
+ * If the user is not authenticated, it redirects to the login page.
+ * @param {{children: import('react').ReactNode}	} param
+ * @returns The children components if the user is authenticated, otherwise a redirect to the login page.
+ */
 const RutaProtegida = ({ children }) => {
-  const { admin } = useAutorizaciones()
+	const { admin } = useAutorizaciones();
 
-  if (!admin) {
-    return <Navigate to="/login" replace />
-  }
-  return children
-}
-export default RutaProtegida
+	if (!admin) {
+		return (
+			<Navigate
+				replace
+				to="/login"
+			/>
+		);
+	}
+
+	return children;
+};
+
+export default RutaProtegida;
