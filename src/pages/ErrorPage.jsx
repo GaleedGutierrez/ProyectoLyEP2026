f4278fae60e5e01e9102bb242666a656ad1f5964
galeed gutierrez <galeedeveloper@gmail.com>
2026-09-13 16:18:53 -0300

    refactor(ErrorPage): simplify component structure

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,3 @@
-const ErrorPage = () => {
-  return (
-    <h1>Error 404 - Página no encontrada</h1>
-  )
-}
+const ErrorPage = () => <h1>Error 404 - Página no encontrada</h1>;
 
-export default ErrorPage
+export default ErrorPage;
