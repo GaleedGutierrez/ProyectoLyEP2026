5f9b5aa07aa6d9bb0dcdbca0f4125225e93cbd38
galeed gutierrez <galeedeveloper@gmail.com>
2026-09-13 16:14:22 -0300

    chore(publicLayout): format code for consistency

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,5 +1,5 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
-const PublicLayout = () => <Outlet />
+const PublicLayout = () => <Outlet />;
 
-export default PublicLayout
+export default PublicLayout;
