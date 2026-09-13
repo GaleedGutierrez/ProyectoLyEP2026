0ce0d6d9d419f6dd83f18a2877b37e289892a576
galeed gutierrez <galeedeveloper@gmail.com>
2026-09-13 16:21:02 -0300

    chore(routes): reorganize imports and format code for consistency

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,26 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
+import Dashboard from '@pages/Dashboard';
+import DetalleCliente from '@pages/DetalleCliente';
+import ErrorPage from '@pages/ErrorPage';
+import ListaClientes from '@pages/ListaClientes';
+import Login from '@pages/Login';
+import { Route, Routes } from 'react-router-dom';
 
-import Login from '../pages/Login'
-import Dashboard from '../pages/Dashboard'
-import ListaClientes from '../pages/ListaClientes'
-import DetalleCliente from '../pages/DetalleCliente'
-import ErrorPage from '../pages/ErrorPage'
-import PublicLayout from '../layouts/PublicLayout'
-import PrivateLayout from '../layouts/PrivateLayout'
+import PrivateLayout from '../layouts/PrivateLayout';
+import PublicLayout from '../layouts/PublicLayout';
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route element={<PublicLayout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-      <Route element={<PrivateLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/clientes" element={<ListaClientes />} />
-        <Route path="/clientes/:id" element={<DetalleCliente />} />
-      </Route>
-    </Routes>
-  )
-}
-export default AppRoutes
+const AppRoutes = () => (
+	<Routes>
+		<Route element={<PublicLayout />}>
+			<Route
+				element={<Login />}
+				path="/login"
+			/>
+			<Route
+				element={<ErrorPage />}
+				path="*"
+			/>
+		</Route>
+		<Route element={<PrivateLayout />}>
+			<Route
+				element={<Dashboard />}
+				path="/"
+			/>
+			<Route
+				element={<ListaClientes />}
+				path="/clientes"
+			/>
+			<Route
+				element={<DetalleCliente />}
+				path="/clientes/:id"
+			/>
+		</Route>
+	</Routes>
+);
+
+export default AppRoutes;
