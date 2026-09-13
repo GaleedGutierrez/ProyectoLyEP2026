170ee3afdc7c0b6c3e281a8743d087ffcc7541d1
galeed gutierrez <galeedeveloper@gmail.com>
2026-09-13 16:13:24 -0300

    chore(autorizacionesContext): configure eslint and prettier for code standardization
    * Added eslint and prettier configurations for consistent code style
    * Improved readability and structure of the AutorizacionesProvider component

--- src/context/AutorizacionesContext.jsx
+++ src/context/AutorizacionesContext.jsx
@@ -1,37 +1,47 @@
-import { createContext, useState, useEffect } from 'react'
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable react-refresh/only-export-components */
+// @ts-nocheck
+/* eslint-disable react/jsx-no-constructed-context-values */
+import { createContext, useEffect, useState } from 'react';
 
-export const AutorizacionesContext = createContext()
+export const AutorizacionesContext = createContext();
 
+/**
+ * A provider component that manages the authentication state of the application.
+ * It provides the current admin user, a function to set the admin user, and a function to log out.
+ * @param {{children: import('react').ReactNode}} param
+ * @returns
+ */
 const AutorizacionesProvider = ({ children }) => {
+	const [admin, setAdmin] = useState(() => {
+		const adminGuardado = localStorage.getItem('admin');
 
-  const [admin, setAdmin] = useState(()=>{
-    const adminGuardado= localStorage.getItem('admin')
-    if(adminGuardado){
-      return JSON.parse(adminGuardado)
-    }
-    return null
-  })
-useEffect(()=>{
-  if(admin){
-    localStorage.setItem(
-      'admin',
-      JSON.stringify(admin)
-    )
-  }else{
-    localStorage.removeItem('admin')
-  }
+		if (adminGuardado) {
+			return JSON.parse(adminGuardado);
+		}
 
-},[admin])
-const cerrarSesion=()=>{
-  setAdmin(null)
-}
-return (
-    <AutorizacionesContext.Provider
-      value={{ admin, setAdmin, cerrarSesion }}
-    >
-      {children}
-    </AutorizacionesContext.Provider>
-  )
-}
+		return null;
+	});
 
-export default AutorizacionesProvider
+	useEffect(() => {
+		if (admin) {
+			localStorage.setItem('admin', JSON.stringify(admin));
+		} else {
+			localStorage.removeItem('admin');
+		}
+	}, [admin]);
+
+	const cerrarSesion = () => {
+		setAdmin(null);
+	};
+
+	return (
+		<AutorizacionesContext.Provider
+			value={{ admin, setAdmin, cerrarSesion }}
+		>
+			{children}
+		</AutorizacionesContext.Provider>
+	);
+};
+
+export default AutorizacionesProvider;
